830a349768d9dcf3b2aac89a2077b7fe3e37a696
Geetha Krishna <Gkrishna@BLR.ALLEGISINDIA.COM>
2025-09-20 14:32:09 -0700

    minor deletions

--- dailytaskcreator/src/App.js
+++ dailytaskcreator/src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
+
 import './App.css';
-import { useState } from 'react';
-import Addtask from './Addtask';
+
+
 
 import DayPlannerLandingPage from './Dayplannerlandingpage';
 
